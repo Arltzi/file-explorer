6fb945872cb70df2b32de028d7c8939a85984536
VFS\pg26lev <arieh@zitron.ca>
2024-11-25 20:05:44 -0800

    just trying to get collision detection when placing an element down done

--- Assets/Scripts/DraggableBlock.cs
+++ Assets/Scripts/DraggableBlock.cs
@@ -44,14 +44,39 @@ public class DraggableBlock : MonoBehaviour
     void Drop()
     {
         selected = false;
-        TrySpawnElement(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z)));
+        if(TrySpawnElement(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z))))
+        {
+            // succeeds at spawning element
+            // TODO: organize by assigning to be a child of a "placed objects" object
+        }
+        else
+        {
+            // fails to spawn element
+            // return icon to beginning, deselect
+            selected = false;
+            transform.position = startPos;
+        }
     }
 
-    void TrySpawnElement(Vector3 worldPos)
+    bool TrySpawnElement(Vector3 worldPos)
     {
-        // TODO: organize by assigning to be a child of a "placed objects" object
-        Instantiate(element, worldPos, Quaternion.identity);
-        Destroy(gameObject);
+        Collider2D firstColl = Physics2D.BoxCast(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z)), element.GetComponent<BoxCollider2D>().size, 0, Vector2.zero).collider;
+        // Debug.Log(firstColl);
+        if(!firstColl)
+        {
+            Instantiate(element, worldPos, Quaternion.identity);
+            Destroy(gameObject);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void ResetIcon()
+    {
+
     }
     
     void DisableSelf()
